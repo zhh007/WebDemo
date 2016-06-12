80027ec9ca2a04be61c6e54a2c97a425ff0ab125
zhh007 <zhh9113@hotmail.com>
2016-06-12 13:47:31 +0800

    update

--- dragulaDemo/gulpfile.js
+++ dragulaDemo/gulpfile.js
@@ -4,28 +4,9 @@ var connect = require('gulp-connect');
 var fs = require("fs");
 var path = require('path');
 var gutil = require('gulp-util');
+var batch = require('gulp-batch');
 
 var controlsPath = 'src/js/controls';
-
-//创建watch任务去检测html文件,其定义了当html改动之后，去调用一个Gulp的Task
-gulp.task('watch', function () {
-  gulp.watch(['./*.html', './src/*.html', './test/*.html'], ['html']);
-});
-
-//使用connect启动一个Web服务器
-gulp.task('connect', function () {
-  connect.server({
-    root: '.',
-    port: 9909,
-    livereload: true
-  });
-});
-
-gulp.task('html', function () {
-  gulp.src('./*.html')
-    .pipe(connect.reload());
-});
-
 function getFolders(dir) {
     return fs.readdirSync(dir)
       .filter(function(file) {
@@ -58,8 +39,37 @@ gulp.task('allcontrols', function() {
    txt += "    }\r\n";
    txt += "  });\r\n";
 
+  console.log('allcontrols doing!');
   return string_src("allcontrols.js", txt)
       .pipe(gulp.dest('src/js/controls/'))
+});
+
+//创建watch任务去检测html文件,其定义了当html改动之后，去调用一个Gulp的Task
+gulp.task('watch', function () {
+  gulp.watch(['./*.html', './src/*.html', './test/*.html'], ['html']);
+  gulp.watch(['**/*.js'], ['js', 'allcontrols']);
+  // gulp.watch('**/*.js', batch(function (events, done) {
+  //       gulp.start('allcontrols', done);
+  //   }));
+});
+
+//使用connect启动一个Web服务器
+gulp.task('connect', function () {
+  connect.server({
+    root: '.',
+    port: 9909,
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+  gulp.src('**/*.js')
+    .pipe(connect.reload());
 });
 
 //运行Gulp时，默认的Task
