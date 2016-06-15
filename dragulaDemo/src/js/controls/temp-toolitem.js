0fa2d4ffd6f0bb643e9f09fd5559f9b10361e0ca
zhh007 <zhh9113@hotmail.com>
2016-06-15 15:05:21 +0800

    update

--- dragulaDemo/src/js/controls/temp-toolitem.js
+++ dragulaDemo/src/js/controls/temp-toolitem.js
@@ -15,33 +15,22 @@ define([
             , initialize: function (options) {
                 this.control = options.control;
                 PubSub.on("NewTempControlMove", this.handleNewTempControlMove, this);
-                //this.constructor.__super__.initialize.call(this);
-                //this.tempTemplate = _.template(_tempTemplate);
             }
             , render: function () {
                 return this.$el.html(this.control.render());
             }
             , handleNewTempControlMove: function (mouseEvent) {
                 this.tempForm = this.$el.find(".ctrl").parent()[0];
-                //console.log(this.tempForm);
-                //debugger;
                 this.halfHeight = Math.floor(this.tempForm.clientHeight / 2);
                 this.halfWidth = Math.floor(this.tempForm.clientWidth / 2);
-                //console.log(this.halfWidth);
                 this.centerOnEvent(mouseEvent);
-                //debugger;
             }
             , centerOnEvent: function (mouseEvent) {
                 var mouseX = mouseEvent.pageX;
                 var mouseY = mouseEvent.pageY;
                 this.tempForm.style.position = 'absolute';
                 this.tempForm.style.top = (mouseY - this.halfHeight) + "px";
-                //var l = (mouseX - this.halfWidth);
                 this.tempForm.style.left = (mouseX - 20) + "px";
-                //console.log(mouseX);
-                // Make sure the element has been drawn and
-                // has height in the dom before triggering.
-                //console.log("toolboxitem");
 
                 PubSub.trigger("ToolboxItemMove", mouseEvent);
             }
